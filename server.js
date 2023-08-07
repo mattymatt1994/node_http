9c1fc5290cae8b1580734841bb62f23ba3baebb7
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-08-07 16:39:34 -0400

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import * as http from "http";
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
@@ -12,37 +14,39 @@ const server = http.createServer((req, res) => {
   const chunks = [];
   req.on("data", (chunk) => {
     chunks.push(chunk);
+    
+    console.log(data);
     //remeber to put "const chunks = []" on the outside
     // so the scope is global
   });
 
   req.on("end", () => {
     console.log("ending", chunks);
+    const data = Buffer.concat(chunks).toString();
     const allThree = {
       url,
       method,
       data,
     };
-    if (url == "/echo" && (method == "POST" || method == "PUT") && chunks > 0) {
-      res.statusCode = 202;
-      res.writeHead(200, { "content-type": "application/json" });
-      res.write(JSON.stringify(allThree));
-      res.end();
-    } else if (url == "/about" && method == "GET") {
-      res.writeHead(200, { "context-type": "text/html" });
-      res.write("<h1>About Matty Matt<h1>");
-      res.end();
-    } else if (url == "/" && (method == "GET" || method == "POST")) {
+    if (url == "/" && (method == "GET" || method == "POST")) {
       res.writeHead(200, { "content-type": "text/html" });
       res.write(`Thank you for ${method.toLowerCase()}ing`);
+      res.end();}
+       else if (url == "/about" && method == "GET") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>About Matty Matt<h1>");
       res.end();
-    } else {
+    } else if (url == "/echo" && (method == "GET" || method == "PUT")) {
+      console.log('echo reached');
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(JSON.stringify(allThree));
+      res.end();
+    } else  {
       res.writeHead(404, { "content-type": "text/html" });
       res.write("ERROR: YOU'VE DOOMED US ALL");
       res.end();
     }
-    const data = JSON.parse(Buffer.concat(chunks).toString());
-    console.log(data);
+   
   });
   //Put all the chunks together and read the message
   //Reconstructing the parts of the message
